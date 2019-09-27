d25efba93abe0b2f9c5ea74273d5666c93fe870e
hzhubingxin <hzhubingxin@corp.netease.com>
2019-09-27 14:20:05 +0800

    bs(node.left, l + 1, h);

--- src/main/java/com/hbx/play/zcytest/binarytree/BinaryTreeAlg.java
+++ src/main/java/com/hbx/play/zcytest/binarytree/BinaryTreeAlg.java
@@ -1107,12 +1107,12 @@ public class BinaryTreeAlg {
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    public static void main(String[] args) {
-        int[] pre = {1, 2, 4, 5, 3, 6, 7};
-        int[] in = {4, 2, 5, 1, 6, 3, 7};
-
-        generatePostArraryByPreAndIn(pre, in);
-    }
+//    public static void main(String[] args) {
+//        int[] pre = {1, 2, 4, 5, 3, 6, 7};
+//        int[] in = {4, 2, 5, 1, 6, 3, 7};
+//
+//        generatePostArraryByPreAndIn(pre, in);
+//    }
 
     /**
      * 根据二叉树的前序和中序生成后续，不需要重建二叉树
@@ -1162,7 +1162,74 @@ public class BinaryTreeAlg {
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    public static void main(String[] args) {
+        Node root = new Node(1);
 
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+
+        Node node6 = new Node(6);
+        Node node7 = new Node(7);
+
+        Node node8 = new Node(8);
+        Node node9 = new Node(9);
+
+        Node node10 = new Node(10);
+
+
+        root.left = node2;
+        root.right = node3;
+
+        node2.left = node4;
+        node2.right = node5;
+
+        node3.left = node6;
+        node3.right = node7;
+
+        node4.left = node8;
+        node4.right = node9;
+
+        node5.left = node10;
+
+        getBTSNodeCount(root);
+    }
+
+    /**
+     * 查询完全二叉树的节点数量
+     *
+     * @param head
+     * @return
+     */
+    public static int getBTSNodeCount(Node head) {
+        if (null == head) {
+            return 0;
+        }
+
+        return bs(head, 1, mostLeftLevel(head, 1)); // 树的高度
+    }
+
+    public static int bs(Node node, int l, int h) {
+        if (l == h) {
+            return 1;
+        }
+        if (mostLeftLevel(node.right, l + 1) == h) {
+            return (1 << (h - 1)) + bs(node.right, l + 1, h);
+        } else {
+            return bs(node.left, l + 1, h) + (1 << (h - l - 1));
+        }
+    }
+
+    public static int mostLeftLevel(Node node, int level) {
+        while (null != node) {
+            level++;
+            node = node.left;
+        }
+
+        return level - 1;
+    }
 
     public static class Node {
         int value;
