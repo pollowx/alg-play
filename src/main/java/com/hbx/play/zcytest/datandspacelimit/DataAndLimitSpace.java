3bff3aa9e187487cbe4a27a6765f2df1934fe611
hzhubingxin <hzhubingxin@corp.netease.com>
2019-10-15 13:19:59 +0800

    岛屿感染问题

--- src/main/java/com/hbx/play/zcytest/datandspacelimit/DataAndLimitSpace.java
+++ src/main/java/com/hbx/play/zcytest/datandspacelimit/DataAndLimitSpace.java
@@ -1,10 +1,5 @@
 package com.hbx.play.zcytest.datandspacelimit;
 
-import com.google.common.collect.Lists;
-import com.google.common.hash.BloomFilter;
-import com.google.common.hash.Funnel;
-import com.google.common.hash.PrimitiveSink;
-
 /**
  * @Auther: bingxin
  * @Date: 2019-10-14 21:32
@@ -17,23 +12,23 @@ public class DataAndLimitSpace {
     // k组hash的结果，如果某个对应的hash值不存在那么改原始值一定不在样本中
     // k组hash的结果，如果某个对应的hash值存在那么改原始值不一定在样本中（是概率问题）
 
-    public static void main(String[] args) {
-        long size = 1000 * 1000;
-
-        BloomFilter<Integer> filter = BloomFilter.create(new Funnel<Integer>() {
-            @Override
-            public void funnel(Integer from, PrimitiveSink into) {
-                return;
-            }
-        }, size);
-
-        for (int i = 0; i <= size; i++) {
-            if (i != 59345) {
-                filter.put(i);
-            }
-        }
-        System.out.println(filter.mightContain(1000000000));
-    }
+//    public static void main(String[] args) {
+//        long size = 1000 * 1000;
+//
+//        BloomFilter<Integer> filter = BloomFilter.create(new Funnel<Integer>() {
+//            @Override
+//            public void funnel(Integer from, PrimitiveSink into) {
+//                return;
+//            }
+//        }, size);
+//
+//        for (int i = 0; i <= size; i++) {
+//            if (i != 59345) {
+//                filter.put(i);
+//            }
+//        }
+//        System.out.println(filter.mightContain(1000000000));
+//    }
 
     // 2GB限制找到20亿中出现次数最多的数字
     // 文件分割，这种考察大数据量的一般都是文件分割，或者机器分割来处理分割后的小文件
@@ -53,6 +48,44 @@ public class DataAndLimitSpace {
 
     // 一致性hash的远离
 
+    // 岛问题
+
+    public static int get连成片的倒数数量(int[][] martrix) {
+        if (null == martrix || martrix.length == 0 || null == martrix[0] || martrix[0].length == 0) {
+            return 0;
+        }
+        // 用感染的思路来解决这个问题，当遇到是1的时候递归地检查它的周围来感染
+        int row = martrix.length;
+        int col = martrix[0].length;
+
+        int res = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (martrix[i][j] == 1) { // 找到了单独的一个
+                    res++;
+                    // 开始感染
+                    infectIsland(martrix, i, j, row, col);
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void infectIsland(int[][] m, int i, int j, int M, int N) {
+        if (i < 0 || i > M || j < 0 || j > N || m[i][j] != 1) {
+            return;
+        }
+        m[i][j] = 2; // 上来先感染掉
+        // 递归
+        infectIsland(m, i + 1, j, M, N);
+        infectIsland(m, i - 1, j, M, N);
+        infectIsland(m, i, j + 1, M, N);
+        infectIsland(m, i, j - 1, M, N);
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 
 }
