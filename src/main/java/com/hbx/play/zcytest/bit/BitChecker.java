15c5898206057deb9173e307d4186ba3968dcf12
hzhubingxin <hzhubingxin@corp.netease.com>
2019-10-15 17:16:23 +0800

    找到数组中只出现1次或者两次的奇数次数字

--- src/main/java/com/hbx/play/zcytest/bit/BitChecker.java
+++ src/main/java/com/hbx/play/zcytest/bit/BitChecker.java
@@ -95,7 +95,7 @@ public class BitChecker {
         int count = 0;
 
         while (n != 0) {
-            n = n & n -1;
+            n = n & n - 1;
             count++;
         }
         return count;
@@ -110,6 +110,48 @@ public class BitChecker {
         n = (n & 0x0000ffff) + ((n >>> 16) & 0x0000ffff);
 
         return n;
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public static void main(String[] args) {
+        int[] arr = {3, 3, 2, 2, 4, 5, 6, 6, 6, 5, 4};
+        System.out.println(找到其他数出现了偶数次但是他出现奇数次的一个数字(arr));
+
+        int[] arrB = {1, 2, 5, 5, 5, 1, 1, 2};
+        找到其他数出现了偶数次但是他出现奇数次的两个数字(arrB);
+    }
+
+    public static int 找到其他数出现了偶数次但是他出现奇数次的一个数字(int[] arr) {
+        if (null == arr || arr.length == 0) {
+            return 0;
+        }
+        int res = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            res = res ^ arr[i];
+        }
+        return res;
+    }
+
+    public static void 找到其他数出现了偶数次但是他出现奇数次的两个数字(int[] arr) {
+        if (null == arr || arr.length == 0) {
+            return;
+        }
+
+        int allEor = 0; // 异或完的结果就是里面的唯二的两个数字的抑或和
+        for (int i = 0; i < arr.length; i++) {
+            allEor ^= arr[i];
+        }
+
+        int rightOne = allEor & (~allEor + 1);
+        int anthor = 0;
+
+        for (int c : arr) {
+            if ((c & rightOne) != 0) {
+                anthor ^= c;
+            }
+        }
+        System.out.println(anthor + " " + (allEor ^ anthor));
     }
 
 }
