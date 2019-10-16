9abfd7708f5970a5460f5902b2509270be68d7a2
hzhubingxin <hzhubingxin@corp.netease.com>
2019-10-16 17:22:01 +0800

    打印旋转矩阵

--- src/main/java/com/hbx/play/zcytest/arraymartrix/ArrayAndMartrix.java
+++ src/main/java/com/hbx/play/zcytest/arraymartrix/ArrayAndMartrix.java
@@ -1,6 +1,9 @@
 package com.hbx.play.zcytest.arraymartrix;
 
+import com.google.common.collect.Lists;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -9,10 +12,6 @@ import java.util.Map;
  * @Description:
  */
 public class ArrayAndMartrix {
-
-    public static void main(String[] args) {
-
-    }
 
     /**
      * 数组值相加等于k的最大子数组长度
@@ -43,6 +42,70 @@ public class ArrayAndMartrix {
             }
         }
         return len;
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public static void main(String[] args) {
+        int[][] array = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+
+        printMartrix(array);
+    }
+
+    /**
+     * 打印旋转矩阵
+     * @param martrix
+     */
+    public static void printMartrix(int[][] martrix) {
+        if (null == martrix || martrix.length == 0 ||
+                null == martrix[0] || martrix[0].length == 0) {
+            return;
+        }
+        // 按照交换的顺序来打印，行转成列
+
+        List<Integer> hpList = Lists.newArrayList();
+        int row = martrix.length; // 二维数组的行数
+
+        while (row != 0) {
+            for (int i = 0; i < martrix[0].length; i++) {
+                hpList.add(martrix[0][i]);
+            }
+
+            martrix = rotateMartrix(martrix);
+            if (null == martrix) {
+                break;
+            }
+            row = martrix.length;
+
+            for (Integer i : hpList) {
+                System.out.print(i + "\t");
+            }
+            System.out.println("");
+            hpList.clear();
+        }
+    }
+
+    /**
+     * 旋转
+     * @param martrix
+     * @return
+     */
+    public static int[][] rotateMartrix(int[][] martrix) {
+        if (null == martrix) {
+            return null;
+        }
+
+        int row = martrix.length;
+        int col = martrix[0].length;
+
+        int[][] res = new int[col][row - 1];
+
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row - 1; j++) {
+                res[i][j] = martrix[j + 1][col - 1 - i];
+            }
+        }
+        return res;
     }
 
 }
