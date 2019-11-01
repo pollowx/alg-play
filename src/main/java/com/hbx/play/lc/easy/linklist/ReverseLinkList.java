633a8e4d1e38d6e059c5e8d7f8ec4f8ed77c32ac
hzhubingxin <hzhubingxin@corp.netease.com>
2019-11-01 15:47:11 +0800

    LC-142. 环形链表 II

--- src/main/java/com/hbx/play/lc/easy/linklist/ReverseLinkList.java
+++ src/main/java/com/hbx/play/lc/easy/linklist/ReverseLinkList.java
@@ -1,5 +1,7 @@
 package com.hbx.play.lc.easy.linklist;
 
+import java.util.HashSet;
+
 /**
  * @Auther: bingxin
  * @Date: 2019-10-31 15:06
@@ -7,20 +9,20 @@ package com.hbx.play.lc.easy.linklist;
  */
 public class ReverseLinkList {
 
-    public static void main(String[] args) {
-        ListNode l1 = new ListNode(1);
-        ListNode l2 = new ListNode(2);
-        ListNode l3 = new ListNode(3);
-        ListNode l4 = new ListNode(4);
-        ListNode l5 = new ListNode(5);
-
-        l1.next = l2;
-        l2.next = l3;
-        l3.next = l4;
-        l4.next = l5;
-
-        reverseList(l1, 2, 4);
-    }
+//    public static void main(String[] args) {
+//        ListNode l1 = new ListNode(1);
+//        ListNode l2 = new ListNode(2);
+//        ListNode l3 = new ListNode(3);
+//        ListNode l4 = new ListNode(4);
+//        ListNode l5 = new ListNode(5);
+//
+//        l1.next = l2;
+//        l2.next = l3;
+//        l3.next = l4;
+//        l4.next = l5;
+//
+//        reverseList(l1, 2, 4);
+//    }
 
     public static ListNode reverseList(ListNode head) {
         if (null == head) {
@@ -104,6 +106,77 @@ public class ReverseLinkList {
             head = next;
         }
         return pre;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(2);
+
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        //l4.next = l2;
+
+        boolean re = hasCycle(l1);
+        boolean re2 = hasCycle2(l1);
+
+        ListNode l = detectCycle(l1);
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        if (null == head) {
+            return false;
+        }
+        HashSet<ListNode> sets = new HashSet<>();
+
+        ListNode cur = head;
+        while (cur != null) {
+            if (cur.next != null && sets.contains(cur.next)) {
+                return true;
+            }
+            if (cur.next != null) {
+                sets.add(cur.next);
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    public static boolean hasCycle2(ListNode head) {
+        if (null == head || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
+    public static ListNode detectCycle(ListNode head) {
+        if (null == head) {
+            return head;
+        }
+        HashSet<ListNode> sets = new HashSet<>();
+        ListNode cur = head;
+        while (cur != null) {
+            if (sets.contains(cur)) {
+                return cur;
+            }
+            sets.add(cur);
+            cur = cur.next;
+        }
+        return null;
     }
 
     public static class ListNode {
