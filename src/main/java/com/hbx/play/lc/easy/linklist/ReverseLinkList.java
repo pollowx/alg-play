02cf1b2f4ab5d7b6b113853258323e87aab5e82e
hzhubingxin <hzhubingxin@corp.netease.com>
2019-11-01 12:09:10 +0800

    反转链表

--- src/main/java/com/hbx/play/lc/easy/linklist/ReverseLinkList.java
+++ src/main/java/com/hbx/play/lc/easy/linklist/ReverseLinkList.java
@@ -87,6 +87,25 @@ public class ReverseLinkList {
         return head;
     }
 
+    public static ListNode reverseList2(ListNode head) {
+        if (null == head) {
+            return head;
+        }
+        ListNode pre = null;
+        ListNode next = null;
+
+        while (head != null) {
+            next = head.next;
+
+            head.next = pre;
+
+            pre = head;
+
+            head = next;
+        }
+        return pre;
+    }
+
     public static class ListNode {
         int val;
         ListNode next;
